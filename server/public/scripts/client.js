780e53d78f858f50af9b4b5618702cfa73615a68
John Hinderer <john.hinderer16@gmail.com>
2020-09-04 19:59:38 -0500

    server has fields

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,11 +2,38 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('in onReady');
-    $(document).on('click', '.equalButton', equate);
+    $(document).on('click', '.equalButton', captureInputs);
+    getItems();
 
 }
 
-function equate(){
-    console.log('in equate');
-    
-}
+function captureInputs(){
+    console.log('in captureInputs');
+    // get user input $ place in object
+    const objSend = {
+        fieldOne: $('.fieldOne').val(),
+        fieldTwo: $('.fieldTwo').val()
+    }
+    console.log('objSend', objSend);
+    // send obj to server via POST thru ajax
+    $.ajax({
+        method: 'POST',
+        url: '/history',
+        data: objSend
+    }).then(function(response){
+        console.log('back from server with:', response);
+        // update DOM with inventory
+        getItems();
+    }).catch(function(err){
+        alert('problem!');
+        console.log(err);
+    }) // end ajax POST
+} // end captureInputs
+
+function getItems(){
+    console.log('in getItems');
+    // select ul & empty
+    // make GET call to server
+    // loop through history
+    // append items to DOM
+} // end getItems
--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 // requires
 const express = require( 'express' );
+const bodyParser = require( 'body-parser' );
 const app = express();
 
 // uses
 app.use( express.static( 'server/public' ));
+app.use(bodyParser.urlencoded({extended: true}));
 
 // globals
 const port = 3000;
@@ -11,4 +13,10 @@ const port = 3000;
 // spin up server
 app.listen(port, ()=>{
     console.log('This is Dr. Mantis Toboggan M.D.', port);
-});// end server up
+}); // end server up
+
+// routes
+app.post( '/history', (req, res)=>{
+    console.log(req.body);
+    res.send('hello I am Swedish plumber');
+}) // end /history POST
